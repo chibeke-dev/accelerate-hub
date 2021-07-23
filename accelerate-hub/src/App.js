ed7bf566b0c8436aef9399d3f3b42906f181fa6f
Chidinma Nwabekee <chibekee17@gmail.com>
2021-07-23 03:54:45 +0100

    redux setup

--- accelerate-hub/src/App.js
+++ accelerate-hub/src/App.js
@@ -6,20 +6,22 @@ import HomePage from './pages/dashboardPages/HomePage'
 
 
 
-import { Switch, Route } from 'react-router-dom';
+
+
+import {  Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
 function App() {
   return (
-       <>
+       
        <Switch>
        <Route exact path="/" component={Home} />
        <Route exact path="/login" component={Login} />
 				<Route exact path="/signup" component={Signup} />
         <Route exact path="/home" component={HomePage} />
     </Switch>
-    </>
+    
   );
 }
 
--- accelerate-hub/src/index.js
+++ accelerate-hub/src/index.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
-import { BrowserRouter } from 'react-router-dom';
+ import { BrowserRouter } from 'react-router-dom';
 import App from './App'
 
 
 
 
 
+
 ReactDOM.render(
-     <>
-      <BrowserRouter>
-         <App />
-     </BrowserRouter>
-    
-    </>,
-  
-  document.getElementById('root')
-);
+    <React.StrictMode>
+        <BrowserRouter>
+        <App />
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
